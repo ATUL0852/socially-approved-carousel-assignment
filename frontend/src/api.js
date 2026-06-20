bb4d8f36f394dfee526cf273a8022c7bb84b0e61
ATUL0852 <111783810+ATUL0852@users.noreply.github.com>
2026-06-21 00:22:19 +0530

    Update unlikeVideo API URL to use BASE_URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -21,7 +21,7 @@ export const unlikeVideo = async (
   videoId
 ) => {
 
-  const response = await fetch("http://localhost:5000/api/unlike",
+  const response = await fetch(`${BASE_URL}/unlike`,
     {
       method: "POST",
       headers: {

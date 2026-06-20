417b373c219055d4a0879de143e67aee90ada7cb
ATUL0852 <111783810+ATUL0852@users.noreply.github.com>
2026-06-21 00:00:29 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use("/videos", express.static("public/videos"));
 
 app.use("/api", videoRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
